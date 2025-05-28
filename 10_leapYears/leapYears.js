49ab7fb9fc215c4782c2b764b4755af2a85bd997
Ellie Deli <131412056+elliedeli@users.noreply.github.com>
2025-05-27 20:24:25 -0500

    Finished assignments

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+     //[year is divisible by 4] AND [year is NOT divisible by 100 OR year IS divisible by 400]
+     return ((year%4==0) && (year%100!==0||year%400==0))
 };
 
 // Do not edit below this line
