95847cddb89e8a974e29bcc1c004229742f56f0b
Ellie Deli <131412056+elliedeli@users.noreply.github.com>
2025-05-27 20:23:54 -0500

    Finished Assignments

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+
+  return Math.round(((fahrTemp-32)*(5/9)).toFixed(1) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  return Math.round((celTemp * (9/5) + 32).toFixed(1)* 10) / 10;
 };
 
 // Do not edit below this line
